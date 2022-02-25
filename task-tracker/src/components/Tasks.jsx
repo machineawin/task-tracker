deb4cfd8fbabeee5c1ab6d248508242914eadf86
machineawin <machineawin@gmail.com>
2022-02-26 07:19:45 +1100

    Create task component

--- task-tracker/src/components/Tasks.jsx
+++ task-tracker/src/components/Tasks.jsx
@@ -1,11 +1,32 @@
 
 const tasks = [
-    
+    {
+        id: 1,
+        text: 'Doctors',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Dentists',
+        day: 'Mar 5th at 3:30pm',
+        reminder: false,
+    },
+    {
+        id: 3,
+        text: 'Nurses',
+        day: 'Apr 5th at 4:30pm',
+        reminder: true,
+    }
 ]
 
 const Tasks = () => {
   return (
-    <div>Tasks</div>
+    <>
+        {tasks.map((task) => (
+            <h3>{task.text}</h3>
+        ) )}
+    </>
   )
 }
 
