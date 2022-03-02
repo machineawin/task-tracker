b08929db3c6593eb815b91ecf858ba5f3d3bdd9d
machineawin <machineawin@gmail.com>
2022-03-02 21:49:16 +1100

    Add delete button FaTimes

--- task-tracker/src/components/Task.jsx
+++ task-tracker/src/components/Task.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { FaTimes } from 'react-icons/fa'
 
-const Task = ( {task} ) => {
+const Task = ( {task, onDelete} ) => {
   return (
     <div className='task'>
-        <h3>{task.text}</h3>
+        <h3>
+            {task.text}
+            <FaTimes onClick={onDelete} style={{ color: 'red', cursor: 'crosshair'}}/>
+        </h3>
         <p>{task.time}</p>
     </div>
   )
